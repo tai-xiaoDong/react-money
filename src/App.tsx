4e7963c17595727bf68c77334d8e845f1927f84e
tai-xiaoDong <672236157@qq.com>
2022-08-11 03:28:35 +0800

    完成导航栏布局

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,91 @@
-import React from 'react';
+import React from "react";
+import styled from "styled-components";
+import {
+  HashRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect
+} from "react-router-dom";
+
+const Wrapper = styled.div`
+  height:100vh;
+  display:flex;
+  flex-direction:column;
+`;
+
+const Main = styled.div`
+
+  flex-grow:1;
+  overflow:auto;
+`;
+
+const Nav = styled.nav`
+  border:1px solid blue;
+  >ul{
+    display:flex;
+    >li{
+      width:33.3333%;
+      text-align:center;
+      padding:16px;
+    }
+  }
+  
+`
 
 function App() {
   return (
-    <div className="App">
-      你好
-    </div>
+    <Router>
+      <Wrapper>
+        <Main>
+          <Switch>
+            <Route path="/tags">
+              <Tags />
+            </Route>
+            <Route path="/money">
+              <Money />
+            </Route>
+            <Route path="/statistics">
+              <Statistics />
+            </Route>
+            <Redirect exact from="/" to="/money" />
+            <Route path="*">
+              <NoMatch />
+            </Route>
+          </Switch>
+        </Main>
+        <Nav>
+          <ul>
+            <li>
+              <Link to="/tags">标签</Link>
+            </li>
+            <li>
+              <Link to="/money">记账</Link>
+            </li>
+            <li>
+              <Link to="/statistics">统计</Link>
+            </li>
+          </ul>
+        </Nav>
+      </Wrapper>
+    </Router >
   );
+}
+
+function Tags() {
+  return <h2>标签页面</h2>;
+}
+
+function Money() {
+  return <h2>记账页面</h2>;
+}
+
+function Statistics() {
+  return <h2>统计页面</h2>;
+}
+
+function NoMatch() {
+  return <h2>你输入的路径有误</h2>
 }
 
 export default App;
