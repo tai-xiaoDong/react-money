acbfe4ff17f86fad372fc4a63d2d0ec160b1b469
tai-xiaoDong <672236157@qq.com>
2022-08-12 09:40:04 +0800

    完成icon

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,5 +1,11 @@
+
 import styled from "styled-components";
 import { Link } from "react-router-dom";
+require('iconfont/money.svg');
+require('iconfont/tags.svg');
+require('iconfont/statistics.svg');
+
+
 
 const NavWrapper = styled.nav`
   line-height:24px;
@@ -9,7 +15,15 @@ const NavWrapper = styled.nav`
     >li{
       width:33.3333%;
       text-align:center;
-      padding:16px;
+      padding:8px 0;
+      display:flex;
+      flex-direction:column;
+      justify-content: center;
+      align-items:center;
+      >icon{
+        width:40px;
+        height:40px
+      }
     }
   }
 `;
@@ -18,17 +32,26 @@ function Nav() {
     return (
         <NavWrapper>
             <ul>
-                <li>
+                <li >
+                    <svg className="icon" >
+                        <use xlinkHref="#tags" />
+                    </svg>
                     <Link to="/tags">标签</Link>
                 </li>
                 <li>
+                    <svg className="icon" >
+                        <use xlinkHref="#money" />
+                    </svg>
                     <Link to="/money">记账</Link>
                 </li>
                 <li>
+                    <svg className="icon" >
+                        <use xlinkHref="#statistics" />
+                    </svg>
                     <Link to="/statistics">统计</Link>
                 </li>
             </ul>
-        </NavWrapper>
+        </NavWrapper >
     )
 }
 
