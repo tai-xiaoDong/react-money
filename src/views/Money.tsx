8dbccd12cf1ba7d4ae97194a91c85ebc9148a849
tai-xiaoDong <672236157@qq.com>
2022-08-13 09:06:03 +0800

    money的CategorySection部分样式完成

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -45,7 +45,28 @@ const NotesSection = styled.section`
     }
 `;
 const CategorySection = styled.section`
-
+    font-size:24px;
+    >ul{
+        display:flex;
+        background:#c4c4c4;
+        >li{
+            width:50%;
+            text-align: center;
+            padding:16px 0;
+            position: relative;
+            &.selected::after{
+                content:'';
+                display: block;
+                height:3px;
+                background: #333;
+                position: absolute;
+                bottom: 0;
+                width: 100%;
+                left: 0;
+            }
+        }
+    }
+    
 `;
 const NumberSection = styled.section`
 
@@ -71,7 +92,7 @@ const Money = () => {
             </NotesSection>
             <CategorySection>
                 <ul>
-                    <li>支出</li>
+                    <li className="selected">支出</li>
                     <li>收入</li>
                 </ul>
             </CategorySection>
