0f513fbdd15a4a9da43149e85de80472eb402eee
tai-xiaoDong <672236157@qq.com>
2022-08-23 19:28:38 +0800

    基本完成Tag页面样式

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -3,6 +3,7 @@ require('iconfont/money.svg');
 require('iconfont/tags.svg');
 require('iconfont/statistics.svg');
 require('iconfont/right.svg');
+require('iconfont/left.svg');
 
 // let importAll = (requireContext: __WebpackModuleApi.RequireContext) => requireContext.keys().forEach(requireContext);
 // try { importAll(require.context('icons', true, /\.svg$/)); } catch (error) { console.log(error); }
--- src/views/Tag.tsx
+++ src/views/Tag.tsx
@@ -1,18 +1,52 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import useTags from 'useTags';
+import Layout from 'components/Layout';
+import Icon from "components/Icon";
+import { Button } from 'components/Button';
+import styled from "styled-components";
+import { Input } from "components/Input";
+
 
 type Params = {
     id: string;
 }
+const Topbar = styled.header`
+    display: flex;
+    justify-content: space-between;
+    line-height: 16px;
+    padding: 8px ;
+    background: white;
+    align-items: center;
+    >span{
+        padding: 8px;
+    }
+`
+
 
 const Tag: React.FC = (props) => {
     const { findTag } = useTags();
     let { id } = useParams<Params>();
-
     const tag = findTag(parseInt(id))
     return (
-        <div>{tag.name}</div>
+        <Layout>
+            <Topbar>
+                <Icon name="left"></Icon>
+                <span>编辑标签</span>
+                <span></span>
+            </Topbar>
+            <div>
+                <label>
+                    <span>标签名</span>
+                    <input type="text" placeholder="请在此处输入标签名" />
+                </label>
+            </div>
+            <div>
+                <Button>删除标签</Button>
+            </div>
+
+
+        </Layout>
     )
 }
 
