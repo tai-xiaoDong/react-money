1801ab83d411558307015cf3fd555e442d710a1d
tai-xiaoDong <672236157@qq.com>
2022-08-25 12:52:21 +0800

    实现 tag页面的回退功能

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -1,20 +1,23 @@
 /*eslint-disable*/
+import cs from 'classnames';
 require('iconfont/money.svg');
 require('iconfont/tags.svg');
 require('iconfont/statistics.svg');
 require('iconfont/right.svg');
 require('iconfont/left.svg');
 
+
 // let importAll = (requireContext: __WebpackModuleApi.RequireContext) => requireContext.keys().forEach(requireContext);
 // try { importAll(require.context('icons', true, /\.svg$/)); } catch (error) { console.log(error); }
 
 type Props = {
-    name: string;
-}
+    name?: string;
+} & React.SVGAttributes<SVGElement>
 
 const Icon = (props: Props) => {
+    const { name, children, className, ...rest } = props;
     return (
-        <svg className="icon" >
+        <svg className={cs('icon', className)} {...rest}>
             <use xlinkHref={'#' + props.name} />
         </svg>)
 }
--- src/views/Tag.tsx
+++ src/views/Tag.tsx
@@ -1,5 +1,6 @@
+/*eslint-disable*/
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useHistory } from "react-router-dom";
 import useTags from 'useTags';
 import Layout from 'components/Layout';
 import Icon from "components/Icon";
@@ -34,35 +35,39 @@ const Tag: React.FC = () => {
     const { findTag, updateTag, deleteTag } = useTags();
     let { id: idString } = useParams<Params>();
     const tag = findTag(parseInt(idString))
-    if (tag) {
-        return (
-            <Layout>
-                <Topbar>
-                    <Icon name="left"></Icon>
-                    <span>编辑标签</span>
-                    <span />
-                </Topbar>
-                <InputWrapper>
-                    <Input
-                        label="标签名:"
-                        type="text"
-                        placeholder="标签名"
-                        value={tag.name}
-                        onChange={(e) => {
-                            updateTag(tag.id, { name: e.target.value })
-                        }}
-                    />
-                </InputWrapper>
-                <Center>
-                    <Button onClick={() => { deleteTag(tag.id) }}>删除标签</Button>
-                </Center>
-            </Layout>
-        )
-    } else {
-        return (
-            <div>标签已经删除</div>
-        )
-    }
+    const history = useHistory();
+    const onClickBack = () => {
+        history.goBack()
+    };
+    return (
+        <Layout>
+            <Topbar>
+                <Icon name="left" onClick={onClickBack} />
+                <span>编辑标签</span>
+                <span />
+            </Topbar>
+            {tag ?
+                <div>
+                    <InputWrapper>
+                        <Input
+                            label="标签名:"
+                            type="text"
+                            placeholder="标签名"
+                            value={tag.name}
+                            onChange={(e) => {
+                                updateTag(tag.id, { name: e.target.value })
+                            }}
+                        />
+                    </InputWrapper>
+                    <Center>
+                        <Button onClick={() => { deleteTag(tag.id) }}>删除标签</Button>
+                    </Center>
+                </div>
+                : <Center>标签已经删除</Center>
+            }
+        </Layout>
+    )
+
 }
 
 export { Tag }
